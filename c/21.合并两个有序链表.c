b6aec4a99428d067c5b3921dd2b22fb89c98b634
Cao Jun <sxha951@gmail.com>
2025-07-31 16:06:54 +0800

    update 0731

--- c/21.合并两个有序链表.c
+++ c/21.合并两个有序链表.c
@@ -14,37 +14,31 @@
  */
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
     struct ListNode dummy;
-    struct ListNode * tail = &dummy;
-    
-    dummy.next = NULL;
+    struct ListNode * cur = &dummy;
 
-    
     while (list1 != NULL && list2 != NULL)
     {
-    if (list1->val <= list2->val)
-    {
-        tail->next = list1;
-        list1 = list1->next;
-
-    }
-    else{
-        tail-> next = list2;
-        list2 = list2->next;
-    }
-        tail = tail -> next;
-
+        if (list1 -> val < list2 -> val)
+        {
+            cur->next = list1;
+            list1 = list1->next;
+        }
+        else{
+            cur->next = list2;
+            list2 = list2 -> next;
+        }
+        cur = cur->next;
     }
     
     if (list1 != NULL)
+    {   
+        cur->next = list1;
+    }
+    else if (list2 != NULL)
     {
-        tail->next = list1;
+        cur->next = list2;
     }
     
-    if (list2 != NULL)
-    {
-        tail->next = list2;
-    }
-
     return dummy.next;
     
 }
--- c/26.删除有序数组中的重复项.c
+++ c/26.删除有序数组中的重复项.c
@@ -6,7 +6,22 @@
 
 // @lc code=start
 int removeDuplicates(int* nums, int numsSize) {
+    if (numsSize == 0) {
+        return 0;
+    }
     
+    int i = 0;
+    
+    for (int j = 0;  j < numsSize; j++)
+    {
+        if (nums[i] != nums[j])
+        {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+
+    return i+1;
 }
 // @lc code=end
 
